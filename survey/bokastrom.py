48c3d1deb2eef3a1b36e0b8c06c0cb6f948723ab
Ian McGreer <imcgreer@gmail.com>
2015-06-11 11:00:58 -0700

    add SIP to PV conversion for sextractor

--- survey/bokastrom.py
+++ survey/bokastrom.py
@@ -8,7 +8,9 @@ import bass
 pxscl = 0.45
 cfgpath = '/project/projectdirs/desi/users/imcgreer/astrometry-index-4200/cfg'
 
-def solve_bass_tile(utDate,fileName,ra,dec,raw=False):
+def solve_bass_tile(utDate,fileName,ra,dec,raw=False,redo=False,
+                    convert2pv=False):
+	sip2pv = os.path.join(os.environ['HOME'],'soft','sip2pv')
 	solveargs = ['solve-field','--config',cfgpath]
 	solveargs += ['--ra','%.7f'%ra,'--dec','%.7f'%dec,'--radius','1.0']
 	solveargs += ['-L','%.2s' % (0.9*pxscl)]
@@ -19,10 +21,14 @@ def solve_bass_tile(utDate,fileName,ra,dec,raw=False):
 	solveargs += ['--crpix-center','-N','none','-U','none',
 	              '-S','none','-M','none',
 	              '--rdls','none','--corr','none']
+	# in order to do SIP->PV conversion for sextractor,
+	# see https://groups.google.com/forum/#!topic/astrometry/qrNW5KtsNCc
+	solveargs += ['--tweak-order','4']
 	if raw:
 		imagepath = os.path.join(bass.bass_data_dir,
 		                         utDate,fileName+'.fits.gz')
 		for extNum in [1,5,9,13]:
+			raise NotImplementedError
 			wcsfn = 'foo%d.wcs' % extNum
 			solvecmd = solveargs + ['--extension','%d'%extNum]
 			solvecmd += ['--wcs',wcsfn]
@@ -37,12 +43,17 @@ def solve_bass_tile(utDate,fileName,ra,dec,raw=False):
 				print ' does not exist; skipping'
 				continue
 			wcsfn = imagepath.replace('.fits','.wcs')
-			if os.path.exists(wcsfn):
+			if not redo and os.path.exists(wcsfn):
 				print wcsfn,' exists, skipping'
 				continue
 			solvecmd = solveargs + ['--wcs',wcsfn,imagepath]
 			subprocess.call(solvecmd)
 			subprocess.call(['missfits','-c','config/missfits.cfg',imagepath])
+			if convert2pv:
+				pvimpath = imagepath.replace('.fits','_pv.fits')
+				if os.path.exists(pvimpath):
+					os.unlink(pvimpath)
+				subprocess.call([sip2pv,'-i',imagepath,'-o',pvimpath])
 
 def solve_bass_tile_byid(tileId,ditherId,imageNum=-1,raw=False):
 	obsdb = bass.load_obsdb()
--- survey/bokextract.py
+++ survey/bokextract.py
@@ -6,21 +6,24 @@ import subprocess
 
 from bass import rdxdir
 
-def sextract(imagepath):
+def sextract(imagepath,frompv=True,redo=False):
 	catpath = imagepath.replace('.fits','.cat.fits')
-	if os.path.exists(catpath):
+	if not redo and os.path.exists(catpath):
 		print catpath,' exists; skipping'
 		return
-	cmd = ['sex','-c','config/default.sex','-CATALOG_NAME',catpath,imagepath]
+	_imagepath = imagepath.replace('.fits','_pv.fits') if frompv else imagepath
+	cmd = ['sex','-c','config/default.sex','-CATALOG_NAME',catpath,_imagepath]
 	subprocess.call(cmd)
 
 def sextract_all():
-	files = sorted(glob.glob(os.path.join(rdxdir,'201504*','ccdproc3',
+	files = sorted(glob.glob(os.path.join(rdxdir,'2015????','ccdproc3',
 	                                      'd????.????_ccd?.wcs')))
 	for f in files:
 		sextract(f.replace('.wcs','.fits'))
 
 
 if __name__=='__main__':
+	import sys
+	redo = 'redo' in sys.argv[1:]
 	sextract_all()
 
