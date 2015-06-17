0c1839474cd08b28eeef672e39134c17ca531402
Ian McGreer <imcgreer@gmail.com>
2015-06-17 11:00:55 +0100

    add strip chart

--- survey/bokdepth.py
+++ survey/bokdepth.py
@@ -3,7 +3,10 @@
 import os
 import re
 import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import ticker
 from astropy.stats import sigma_clip
+import astropy.io.ascii as ascii_io
 import fitsio
 import bass
 
@@ -50,6 +53,34 @@ def calc_depth_all():
 			outf.write('%8.1f %8.1f %8.1f\n' % 
 			     (depthstat['bias'],depthstat['medsky'],depthstat['rmssky']))
 	outf.close()
+
+def read_image_stats():
+	imstat = ascii_io.read('imagestat.dat',
+	                       names=['ti','utDate','fileName','expTime',
+	                              'biasADU','skyADU','rmsADU'])
+	return imstat
+
+def strip_charts():
+	imstat = read_image_stats()
+	# XXX use per-image zeropoints once available
+	zpt = 25.55
+	skymag = -2.5*np.log10(imstat['skyADU']/imstat['expTime']*0.455**-2) + zpt
+	plt.figure(figsize=(10,4))
+	plt.subplots_adjust(0.06,0.13,0.99,0.98)
+	ax1 = plt.subplot(111)
+	ax1.plot(skymag)
+	utds,ii = np.unique(imstat['utDate'],return_index=True)
+	for i,utd in zip(ii,utds):
+		offset = 1.5 if np.sum(imstat['utDate']==utd) < 30 else 0.2
+		ax1.text(i,skymag[i]-offset,imstat['utDate'][i],size=8,
+		         rotation='vertical',ha='left',va='bottom')
+	ax1.axvspan(ii[utds==20150305],ii[utds==20150313],color='c',alpha=0.5)
+	ax1.axhline(22.1,c='r')
+	ax1.yaxis.set_minor_locator(ticker.MultipleLocator(0.2))
+	ax1.set_ylim(22.5,17)
+	ax1.set_xlim(-10,6200)
+	ax1.set_xlabel('image number')
+	ax1.set_ylabel('g sky brightness [mag/asec2]')
 
 if __name__=='__main__':
 	calc_depth_all()
