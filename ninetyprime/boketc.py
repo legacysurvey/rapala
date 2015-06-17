d696fa31533c4bb962ba69912d27396a4004195c
Ian McGreer <imcgreer@gmail.com>
2015-06-17 15:02:48 +0100

    run ETC from command line

--- ninetyprime/boketc.py
+++ ninetyprime/boketc.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from math import sqrt,pi
+from math import sqrt,pi,log10
 
 # extinction parameters
 k_ext = {'g':0.17,'r':0.10}
@@ -36,8 +36,8 @@ def _exp_config(band,mag=None,zpt0=None,sky=None,fwhm=None,
 		fwhm = bok_medianFWHM[band]
 	#
 	f0 = 10**(-0.4*(mag - zpt0)) * G
-	#skyflux = 10**(-0.4*(sky - zpt0)) * p**2 * G
-	skyflux = 6.6 # from images
+	skyflux = 10**(-0.4*(sky - bok_zpt0_am13[band])) * p**2 * G
+	#skyflux = 6.6 # from images
 	sig_sky = sqrt(skyflux)
 	sig_readout = bok_rdnoise
 	sig_seeing = fwhm / 2.355
@@ -62,9 +62,9 @@ def targalt_eqn1(band,SNR=5.,ndith=3,**kwargs):
 	         ( 1./2 + sqrt( 1./4 + ndith/(SNR**2 * NEA * (sig_sky/f0)**2) *
 	                                    (sig_readout/sig_sky)**2 ) )
 	if kwargs.get('verbose',False):
-		print 'calculated exposure time: %.2f' % T_exp
 		print 'source counts: %.1f e-  %.1f ADU' % (f0*T_exp,f0*T_exp/G)
 		print 'sky counts: %.1f e-  %.1f ADU' % (skyflux*T_exp,skyflux*T_exp/G)
+		print 'calculated exposure time: %.2f' % T_exp
 	return T_exp
 
 def targalt_verify(band):
@@ -76,9 +76,9 @@ def snr_singleexposure(band,mag,texp,**kwargs):
 	snr = ( f0*texp / 
 	             sqrt( NEA * (skyflux*texp + sig_readout**2) ) )
 	if kwargs.get('verbose',False):
-		print 'calculated SNR: %.2f' % snr
 		print 'source counts: %.1f e-  %.1f ADU' % (f0*texp,f0*texp/G)
 		print 'sky counts: %.1f e-  %.1f ADU' % (skyflux*texp,skyflux*texp/G)
+		print 'calculated SNR: %.2f' % snr
 	return snr
 
 def texp_onsky(band,airmass,ebv,skyextinction,mag=None,**kwargs):
@@ -89,4 +89,39 @@ def texp_onsky(band,airmass,ebv,skyextinction,mag=None,**kwargs):
 	# zero point is defined at AM=1
 	mag += k_ext[band]*(airmass-1.0) + A_ext[band]*ebv + skyextinction
 	return targalt_eqn1(band,mag=mag,**kwargs)
+
+if __name__=='__main__':
+	import sys,argparse
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-b','--band',default='g',
+	                    help='bandpass [default=g]')
+	parser.add_argument('-a','--airmass',type=float,default=1.0,
+	                    help='airmass [default=1.0]')
+	parser.add_argument('-e','--ebv',type=float,default=0.02,
+	                    help='Galactic E(B-V) [default=0.02]')
+	parser.add_argument('-x','--skyextinction',type=float,default=None,
+	                    help='Sky extinction (mags) [default=0.0]')
+	parser.add_argument('-T','--transparency',type=float,default=None,
+	                    help='Linear transparency (alternative to sky extinction)) [default=1.0]')
+	parser.add_argument('-s','--skybackground',type=float,default=None,
+	                    help='Sky background (mag/asec^2) [default=nominal KPNO]')
+	parser.add_argument('-f','--fwhm',type=float,default=None,
+	                    help='FWHM (arcsec) [default=nominal Bok]')
+	parser.add_argument('-S','--signaltonoise',type=float,default=5.0,
+	                    help='desired SNR [default=5.0]')
+	parser.add_argument('-m','--magnitude',type=float,default=None,
+	                    help='object magnitude (AB) [default=nominal DESI 5sig limit]')
+	parser.add_argument('-z','--zeropoint',type=float,default=None,
+	                    help='zeropoint (AB) [default=nominal Bok]')
+	args = parser.parse_args()
+	skyextinction = args.skyextinction
+	if skyextinction is None:
+		if args.transparency is not None:
+			skyextinction = -2.5*log10(args.transparency)
+		else:
+			skyextinction = 0.0
+	texp_onsky(args.band,args.airmass,args.ebv,skyextinction,
+	           sky=args.skybackground,fwhm=args.fwhm,SNR=args.signaltonoise,
+	           zpt0=args.zeropoint,mag=args.magnitude,
+	           verbose=True)
 
