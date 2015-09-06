f9f825dcf5fb7b821b72615dfcdbef13d9ff6bfa
Ian McGreer <ian@Ians-MacBook-Pro.local>
2015-09-06 11:11:41 -0700

    fix bugs with findinig and growing bad pixels

--- bokpipe/badpixels.py
+++ bokpipe/badpixels.py
@@ -8,27 +8,39 @@ from astropy.stats import sigma_clip
 
 from bokutil import rebin,BokMefImage
 
+import fitsio
+
+
 def build_mask_from_flat(flatFn,outFn,**kwargs):
-	loCut = kwargs.get('lo_cut',0.8)
-	hiCut = kwargs.get('hi_cut',1.2)
+	loCut,hiCut = kwargs.get('good_range',(0.8,1.2))
+	loCut2,hiCut2 = kwargs.get('grow_range',(0.9,1.1))
 	nbin = kwargs.get('nbin',32)
+	flatName = kwargs.get('normed_flat_file')
 	bpMask = BokMefImage(flatFn,output_file=outFn,**kwargs)
+	if flatName is not None:
+		if os.path.exists(flatName):
+			os.unlink(flatName)
+		normedFlat = fitsio.FITS(flatName,'rw')
+		normedFlat.write(None,header=fitsio.read_header(flatFn,0))
 	for data,hdr in bpMask:
 		ny,nx = data.shape
 		x = np.arange(nbin/2,nx,nbin)
 		y = np.arange(nbin/2,ny,nbin)
-		im = np.ma.masked_array(data,mask=((data < 0.4) & (data>1.5)))
+		im = np.ma.masked_array(data,mask=((data < 0.4) | (data>1.5)))
 		binnedIm = rebin(im,nbin)
 		binnedIm = sigma_clip(binnedIm,axis=-1,iters=3,sig=2.2,
-		                      cenfunc=np.ma.mean).mean(axis=-1).filled(0)
+		                      cenfunc=np.ma.mean).mean(axis=-1).filled(1)
 		spfit = RectBivariateSpline(x,y,binnedIm.T,s=1)
 		gradientIm = spfit(np.arange(nx),np.arange(ny)).T
 		im /= gradientIm
-		#bpMask.update(im,hdr)
-		badpix = (im < 0.8) | (im > 1.2)
-		badpix |= binary_dilation(badpix,mask=((im<0.95)|(im>1.05)),
+		if flatName is not None:
+			normedFlat.write(im,extname=bpMask.curExtName,header=hdr)
+		badpix = (im < loCut) | (im > hiCut)
+		badpix |= binary_dilation(badpix,mask=((im<loCut2)|(im>hiCut2)),
 		                          iterations=0)
-		badpix |= binary_closing(badpix)
+		badpix |= binary_closing(badpix,iterations=0)
 		bpMask.update(badpix.astype(np.int16),hdr)
 	bpMask.close()
+	if flatName is not None:
+		normedFlat.close()
 
