fix bug with masking in sky subtraction

bokpipe/bokproc.py
@@ -53,6 +53,8 @@ def make_fov_image(fov,pngfn,**kwargs):
 	h = 0.455
 	if maskFile is not None:
 		maskFits = fitsio.FITS(maskFile)
+	input_vmin = kwargs.get('vmin')
+	input_vmax = kwargs.get('vmax')
 	fig = plt.figure(figsize=(6,6.5))
 	cax = fig.add_axes([0.1,0.04,0.8,0.01])
 	for n,ccd in enumerate(['CCD2','CCD4','CCD1','CCD3']):
@@ -61,10 +63,16 @@ def make_fov_image(fov,pngfn,**kwargs):
 			im = np.ma.masked_array(im,maskFits[ccd][:,:].astype(bool))
 		if n == 0:
 			i1,i2 = 100//fov['nbin'],1500//fov['nbin']
-			background = sigma_clip(im[i1:i2,i1:i2],iters=3,sig=2.2)
-			m,s = background.mean(),background.std()
-			print m,s,m-losig*s,m+hisig*s
-			norm = colors.Normalize(vmin=m-losig*s,vmax=m+hisig*s)
+			if input_vmin is None and input_vmax is None:
+				background = sigma_clip(im[i1:i2,i1:i2],iters=3,sig=2.2)
+				m,s = background.mean(),background.std()
+				print m,s,m-losig*s,m+hisig*s
+				vmin = input_vmin if input_vmin is not None else m-losig*s
+				vmax = input_vmax if input_vmax is not None else m+hisig*s
+			else:
+				vmin = input_vmin
+				vmax = input_vmax
+			norm = colors.Normalize(vmin=vmin,vmax=vmax)
 		if im.ndim == 3:
 			im = im.mean(axis=-1)
 		x = fov[ccd]['x']
@@ -91,7 +99,7 @@ def make_fov_image(fov,pngfn,**kwargs):
 	fig.text(0.5,0.99,title,ha='center',va='top',size=12)
 
 def make_fov_image_fromfile(fileName,pngfn,nbin=1,coordsys='sky',**kwargs):
-	fits = bokutil.BokMefImage(fileName,mask_file=kwargs.get('mask'))
+	fits = bokutil.BokMefImage(fileName,mask_file=kwargs.get('mask'),read_only=True)
 	fov = fits.make_fov_image(nbin,coordsys)
 	fov['file'] = fileName
 	return make_fov_image(fov,pngfn,**kwargs)
@@ -107,7 +115,10 @@ def bok_polyfit(fits,nbin,order,writeImg=False):
 		im = clippedIm.mean(axis=-1)
 		#im = binnedIm[ccd]['im'].mean(axis=-1)
 		nbad = clippedIm.mask.sum(axis=-1)
-		too_few_pixels = nbad < nbin*2//3
+		#too_few_pixels = nbad < nbin*2//3
+		too_few_pixels = nbad > nbin**2//3
+		#print 'biggest: ',nbad.max(),binnedIm[ccd]['im'].shape
+		#too_few_pixels = nbad > 1e10
 		ii = np.where(~too_few_pixels)
 		im[too_few_pixels] = np.ma.masked
 		X.append(binnedIm[ccd]['x'][ii])