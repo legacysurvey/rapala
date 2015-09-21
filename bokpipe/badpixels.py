testing masking and bad pixel fixing

bokpipe/badpixels.py
@@ -12,8 +12,8 @@ import fitsio
 
 
 def build_mask_from_flat(flatFn,outFn,**kwargs):
-	loCut,hiCut = kwargs.get('good_range',(0.8,1.2))
-	loCut2,hiCut2 = kwargs.get('grow_range',(0.9,1.1))
+	loCut,hiCut = kwargs.get('good_range',(0.92,1.08))
+	loCut2,hiCut2 = kwargs.get('grow_range',(0.97,1.03))
 	nbin = kwargs.get('nbin',32)
 	flatName = kwargs.get('normed_flat_file')
 	bpMask = BokMefImage(flatFn,output_file=outFn,**kwargs)
@@ -22,7 +22,7 @@ def build_mask_from_flat(flatFn,outFn,**kwargs):
 			os.unlink(flatName)
 		normedFlat = fitsio.FITS(flatName,'rw')
 		normedFlat.write(None,header=fitsio.read_header(flatFn,0))
-	for data,hdr in bpMask:
+	for extName,data,hdr in bpMask:
 		ny,nx = data.shape
 		x = np.arange(nbin/2,nx,nbin)
 		y = np.arange(nbin/2,ny,nbin)
@@ -34,7 +34,7 @@ def build_mask_from_flat(flatFn,outFn,**kwargs):
 		gradientIm = spfit(np.arange(nx),np.arange(ny)).T
 		im /= gradientIm
 		if flatName is not None:
-			normedFlat.write(im,extname=bpMask.curExtName,header=hdr)
+			normedFlat.write(im,extname=extName,header=hdr)
 		badpix = (im < loCut) | (im > hiCut)
 		badpix |= binary_dilation(badpix,mask=((im<loCut2)|(im>hiCut2)),
 		                          iterations=0)
@@ -47,8 +47,8 @@ def build_mask_from_flat(flatFn,outFn,**kwargs):
 def make_sextractor_gain_map(flatFn,bpMaskFn,gainMapFn,**kwargs):
 	flat = BokMefImage(flatFn,output_file=gainMapFn,**kwargs)
 	mask = fitsio.FITS(bpMaskFn)
-	for data,hdr in flat:
+	for extName,data,hdr in flat:
 		# invert the mask, makes bad pixels = 0
-		data *= (1 - mask[flat.curExtName].read())
+		data *= (1 - mask[extName].read())
 		flat.update(data,hdr)
 	flat.close()

bokpipe/bokproc.py
@@ -42,10 +42,32 @@ nominal_gain = np.array(
 # XXX
 configdir = os.environ['BOKPIPE']+'/config/'
 
+from scipy.interpolate import interp1d,interp2d
+
 def interpolate_masked_pixels(data,along='twod',method='linear'):
-	y,x = np.indices(data.shape)
-	interpfun = griddata([y[~data.mask],x[~data.mask]],data[~data.mask],
-	                     (y,x),method=method)
+	if along=='rows':
+		xx = np.arange(data.shape[1])
+		for i in range(data.shape[0]):
+			interpFun = interp1d(xx[~data.mask[i]],
+			                     data.data[i][~data.mask[i]],kind='linear',
+			                     bounds_error=False,fill_value=None)
+			data.data[i][data.mask[i]] = interpFun(xx[data.mask[i]])
+			interpData = data.data
+	elif along=='twod':
+		y,x = np.indices(data.shape)
+		yy = np.arange(data.shape[0])
+		xx = np.arange(data.shape[1])
+		interpFun = interp2d(x[~data.mask],y[~data.mask],
+		                     data.data[~data.mask],kind='linear',
+		                     bounds_error=False,fill_value=None)
+		data.data[data.mask] = interpFun(x[data.mask],y[data.mask])
+		print interpData.shape
+		#interpData = griddata((y[~data.mask],x[~data.mask]),
+		#                      data.data[~data.mask],
+		#                      (yy,xx),method=method)
+	else:
+		raise ValueError
+	return interpData
 
 def make_fov_image(fov,pngfn,**kwargs):
 	import matplotlib.pyplot as plt
@@ -267,6 +289,7 @@ class BokCCDProcess(bokutil.BokProcess):
 		if self.flatFits is not None:
 			data /= self.flatFits[extName][:,:]
 		if self.fixPix:
+			print 'fixing pixels on extension ',extName
 			data = interpolate_masked_pixels(data,along=self.fixPixAlong,
 			                                 method=self.fixPixMethod)
 		hdr['BIASFILE'] = str(self.biasFile)