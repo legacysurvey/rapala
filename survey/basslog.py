c6e2a7e47dfa19990fdac76f704203a24c323543
Ian McGreer <imcgreer@gmail.com>
2015-10-04 09:12:53 -0700

    add overscan rows to log

--- survey/basslog.py
+++ survey/basslog.py
@@ -10,18 +10,14 @@ from collections import OrderedDict
 data_dir = os.environ.get('BOKDATADIR',
                      '/global/project/projectdirs/cosmo/staging/bok')
 
-def boklog(utdate,logdir='./'):
-	files = glob.glob(os.path.join(data_dir,utdate,'d????.????.fits.gz'))
-	files.sort()
-	logfn = logdir+'bass.%s.log' % utdate
-	if os.path.exists(logfn) or len(files)==0:
-		return
+def boklog(logfn,files):
 	logf = open(logfn,'w')
 	nfiles = len(files)
 	for i,f in enumerate(files):
 		try:
 			#h = fitsio.read_header(f,0)
 			h = fits.getheader(f)
+			h1 = fits.getheader(f,1)
 		except:
 			print 'ERROR: failed to open file: ',f
 			continue
@@ -43,13 +39,23 @@ def boklog(utdate,logdir='./'):
 			if np.abs(airmass-h['AIRMASS']) > 0.1:
 				print airmass,h['AIRMASS'],h['ELEVAT']
 				raise ValueError
-		logf.write('%10s %14s %6s %5.1f %7s %25s %6.3f\n' %
+		ccdsum = h['CCDSUM'].strip()
+		novscanrows = h1['OVRSCAN2']
+		logf.write('%10s %14s %6s %5.1f %7s %25s %6.3f %4s %3d\n' %
 		           (fn,h['UT'],filt,h['EXPTIME'],
-		            imageType,objname,airmass))
+		            imageType,objname,airmass,ccdsum,novscanrows))
 		sys.stdout.write("\r%d/%d" % (i+1,nfiles))
 		sys.stdout.flush()
 	logf.close()
 	print
+
+def basslog(utdate,logdir='./'):
+	files = glob.glob(os.path.join(data_dir,utdate,'d????.????.fits.gz'))
+	files.sort()
+	logfn = logdir+'bass.%s.log' % utdate
+	if os.path.exists(logfn) or len(files)==0:
+		return
+	boklog(logfn,files)
 
 def log_all(logdir='./'):
 	utdates = [os.path.basename(d) 
@@ -57,7 +63,7 @@ def log_all(logdir='./'):
 	utdates.sort()
 	for utdate in utdates:
 		print 'generating log for ',utdate,'...'
-		boklog(utdate,logdir)
+		basslog(utdate,logdir)
 
 def load_Bok_log(utdate,logdir='./'):
 	'''Load an observing log from a single night.'''
@@ -98,8 +104,15 @@ def overhead_times(utdate,logdir='./'):
 	outf.close()
 
 if __name__=='__main__':
+	#basslog('20150909','logs/')
 	#log_all()
-	for utd in ['20150107','20150117','20150204','20150205','20150211'
-	            '20150305','20150324','20150419','20150426']:
-		overhead_times(utd,'logs/')
+	#for utd in ['20150107','20150117','20150204','20150205','20150211'
+	#            '20150305','20150324','20150419','20150426']:
+	#	overhead_times(utd,'logs/')
+	if True:
+		files = glob.glob(os.path.join(data_dir,'20150909','bss','20150908',
+		                  'd????.????.fits.gz'))
+		files.sort()
+		logfn = 'logs/'+'bss.%s.log' % '20150909'
+		boklog(logfn,files)
 
