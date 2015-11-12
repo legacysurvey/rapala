3767ccd60c2a0a3e8edd9007bf5de43ce68457c4
Ian McGreer <imcgreer@gmail.com>
2015-11-12 10:02:22 -0700

    accept a file list argument

--- bokpipe/tools/bokcombine.py
+++ bokpipe/tools/bokcombine.py
@@ -16,7 +16,15 @@ if __name__=='__main__':
 		stackFun = bokutil.ClippedMeanStack(**stackPars)
 	elif method == 'median':
 		stackFun = bokutil.MedianStack(**stackPars)
-	print 'input: ',inputFiles
+	_inputFiles = []
+	# accept an IRAF-style list from a file if args starts with '@'
+	for f in inputFiles:
+		if f.startswith('@'):
+			with open(f[1:]) as _f:
+				_inputFiles.extend(_f.read().splitlines())
+		else:
+			_inputFiles.append(f)
+	print 'input: ',_inputFiles
 	print 'output: ',outputFile
 	stackFun.stack(inputFiles,outputFile)
 
