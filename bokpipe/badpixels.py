dac17015831b00ce32f88aff54fb182165c3895f
Ian McGreer <imcgreer@gmail.com>
2015-11-13 13:12:42 -0700

    don't normalize flat here

--- bokpipe/badpixels.py
+++ bokpipe/badpixels.py
@@ -18,43 +18,9 @@ class BadPixelMaskFromFlats(BokProcess):
 		kwargs.setdefault('header_key','BPMSK')
 		self.loCut,self.hiCut = kwargs.get('good_range',(0.90,1.10))
 		self.loCut2,self.hiCut2 = kwargs.get('grow_range',(0.95,1.05))
-		self.nbin = kwargs.get('nbin',32)
-		self.flatName = kwargs.get('normed_flat_file')
-		self.flatFitName = kwargs.get('normed_flat_fit_file')
-		self.binnedFlatName = kwargs.get('binned_flat_file')
 		self.addBadCols = kwargs.get('add_bad_cols',True)
 		self.noConvert = True  # casting to unsigned int below
-		self.normedFlat = None
-		self.normedFlatFit = None
-		self.binnedFlat = None
-	def _preprocess(self,fits,f):
-		if self.flatName is not None:
-			if os.path.exists(self.flatName):
-				os.unlink(self.flatName)
-			self.normedFlat = fitsio.FITS(self.flatName,'rw')
-			self.normedFlat.write(None,header=fits.get_header(0))
-		if self.flatFitName is not None:
-			if os.path.exists(self.flatFitName):
-				os.unlink(self.flatFitName)
-			self.normedFlatFit = fitsio.FITS(self.flatFitName,'rw')
-			self.normedFlatFit.write(None,header=fits.get_header(0))
-		if self.binnedFlatName is not None:
-			if os.path.exists(self.binnedFlatName):
-				os.unlink(self.binnedFlatName)
-			self.binnedFlat = fitsio.FITS(self.binnedFlatName,'rw')
-			self.binnedFlat.write(None,header=fits.get_header(0))
-	def _finish(self):
-		if self.normedFlat is not None:
-			self.normedFlat.close()
-			self.normedFlat = None
-		if self.normedFlatFit is not None:
-			self.normedFlatFit.close()
-			self.normedFlatFit = None
-		if self.binnedFlat is not None:
-			self.binnedFlat.close()
-			self.binnedFlat = None
 	def process_hdu(self,extName,data,hdr):
-		ny,nx = data.shape
 		im = np.ma.masked_array(data,mask=((data<0.5) | (data>1.5)))
 		# mask the edges
 		margin = 15
@@ -65,23 +31,6 @@ class BadPixelMaskFromFlats(BokProcess):
 		if extName == 'IM9':
 			# this one has a long bad strip
 			im.mask[:,:50] = True
-		binnedIm = rebin(im,self.nbin)
-		binnedIm = sigma_clip(binnedIm,axis=-1,iters=3,sig=2.2,
-		                      cenfunc=np.ma.mean).mean(axis=-1).filled(1)
-		x = np.arange(self.nbin/2,nx,self.nbin)
-		y = np.arange(self.nbin/2,ny,self.nbin)
-		spfit = RectBivariateSpline(x,y,binnedIm.T,s=1)
-		gradientIm = spfit(np.arange(nx),np.arange(ny)).T
-		im /= gradientIm
-		if self.normedFlat is not None:
-			self.normedFlat.write(im.astype(np.float32),
-			                      extname=extName,header=hdr)
-		if self.normedFlatFit is not None:
-			self.normedFlatFit.write(gradientIm.astype(np.float32),
-			                         extname=extName,header=hdr)
-		if self.binnedFlat is not None:
-			self.binnedFlat.write(binnedIm.astype(np.float32),
-			                      extname=extName,header=hdr)
 		badpix = (im < self.loCut) | (im > self.hiCut)
 		badpix |= binary_dilation(badpix,
 		                          mask=((im<self.loCut2)|(im>self.hiCut2)),
