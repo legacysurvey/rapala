65a64fa96a6fe21458b9634f2d2bea868e44c736
Ian McGreer <imcgreer@gmail.com>
2016-01-26 20:11:29 -0700

    start rewrite to work with quickproc images

--- survey/bokastrom.py
+++ survey/bokastrom.py
@@ -5,11 +5,22 @@ import subprocess
 import numpy as np
 import bass
 
+from astropy import units as u
+from astropy.coordinates import SkyCoord
+
 pxscl = 0.45
 cfgpath = '/project/projectdirs/desi/users/imcgreer/astrometry-index-4200/cfg'
 
-def solve_bass_tile(utDate,fileName,ra,dec,raw=False,redo=False,
-                    convert2pv=False):
+def solve_bass_tile(imagepath,ra=None,dec=None,redo=False,
+                    convert2pv=False,extns='all'):
+	if not os.path.exists(imagepath):
+		print imagepath,' does not exist; skipping'
+		return
+	if ra is None:
+		h = fitsio.read_header(imagepath,0)
+		c = SkyCoord(h['RA']+' '+h['DEC'],unit=(u.hourangle,u.deg))
+		ra = c.ra.value
+		dec = c.dec.value
 	sip2pv = os.path.join(os.environ['HOME'],'soft','sip2pv')
 	solveargs = ['solve-field','--config',cfgpath]
 	solveargs += ['--ra','%.7f'%ra,'--dec','%.7f'%dec,'--radius','1.0']
@@ -21,39 +32,26 @@ def solve_bass_tile(utDate,fileName,ra,dec,raw=False,redo=False,
 	solveargs += ['--crpix-center','-N','none','-U','none',
 	              '-S','none','-M','none',
 	              '--rdls','none','--corr','none']
-	# in order to do SIP->PV conversion for sextractor,
-	# see https://groups.google.com/forum/#!topic/astrometry/qrNW5KtsNCc
-	solveargs += ['--tweak-order','4']
-	if raw:
-		imagepath = os.path.join(bass.bass_data_dir,
-		                         utDate,fileName+'.fits.gz')
-		for extNum in [1,5,9,13]:
-			raise NotImplementedError
-			wcsfn = 'foo%d.wcs' % extNum
-			solvecmd = solveargs + ['--extension','%d'%extNum]
-			solvecmd += ['--wcs',wcsfn]
-			solvecmd += [imagepath]
-			print ' '.join(solvecmd)
-	else:
-		for ccdNum in range(1,5):
-			imagepath = os.path.join(bass.rdxdir,utDate,'ccdproc3',
-			                         fileName+'_ccd%d.fits'%ccdNum)
-			if not os.path.exists(imagepath):
-				print utDate,fileName,' ccd #',ccdNum,
-				print ' does not exist; skipping'
-				continue
-			wcsfn = imagepath.replace('.fits','.wcs')
-			if not redo and os.path.exists(wcsfn):
-				print wcsfn,' exists, skipping'
-				continue
-			solvecmd = solveargs + ['--wcs',wcsfn,imagepath]
-			subprocess.call(solvecmd)
-			subprocess.call(['missfits','-c','config/missfits.cfg',imagepath])
-			if convert2pv:
-				pvimpath = imagepath.replace('.fits','_pv.fits')
-				if os.path.exists(pvimpath):
-					os.unlink(pvimpath)
-				subprocess.call([sip2pv,'-i',imagepath,'-o',pvimpath])
+	if convert2pv:
+		# in order to do SIP->PV conversion for sextractor;
+		# see https://groups.google.com/forum/#!topic/astrometry/qrNW5KtsNCc
+		solveargs += ['--tweak-order','4']
+	if extns='all':
+		extns = [1,2,3,4]
+	for extNum in extns:
+		wcsfn = imagepath.replace('.fits','_ext%s.wcs'%extNum)
+		solvecmd = solveargs + ['--extension','%d'%extNum]
+		if not redo and os.path.exists(wcsfn):
+			print wcsfn,' exists, skipping'
+			continue
+		solvecmd += ['--wcs',wcsfn,imagepath]
+		subprocess.call(solvecmd)
+		subprocess.call(['missfits','-c','config/missfits.cfg',imagepath])
+		if convert2pv:
+			pvimpath = imagepath.replace('.fits','_pv.fits')
+			if os.path.exists(pvimpath):
+				os.unlink(pvimpath)
+			subprocess.call([sip2pv,'-i',imagepath,'-o',pvimpath])
 
 def solve_bass_tile_byid(tileId,ditherId,imageNum=-1,raw=False):
 	obsdb = bass.load_obsdb()
