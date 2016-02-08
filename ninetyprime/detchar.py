6ad0bd67ce479266d08a67c19fd1e619e4cb7e77
Ian McGreer <imcgreer@gmail.com>
2016-02-08 16:17:27 -0700

    diagnostic plots from nightly checks

--- ninetyprime/detchar.py
+++ ninetyprime/detchar.py
@@ -5,8 +5,10 @@ from glob import glob
 import numpy as np
 import fitsio
 from astropy.stats import sigma_clip
+from astropy.table import Table,vstack
 
 from bokpipe.bokproc import ampOrder
+from bokpipe.bokutil import stats_region,array_clip
 
 import matplotlib.pyplot as plt
 from matplotlib import ticker
@@ -14,12 +16,11 @@ from matplotlib import ticker
 def _data2arr(data,minVal=0,maxVal=65335,clip=True):
 	arr = np.ma.masked_array(data.astype(np.float32),
 	                         mask=((data<minVal)|(data>maxVal)))
-	return sigma_clip(arr,iters=1,sigma=5.0)
+	return array_clip(arr,clip_iters=2,clip_sig=5.0)
 
-def calc_gain_rdnoise(biases,flats,margin=500):
+def calc_gain_rdnoise(biases,flats):
 	rv = []
-	x1,x2 = margin,-margin
-	y1,y2 = margin,-margin
+	s = stats_region('amp_corner_ccdcenter_1024')
 	for files in zip(biases[:-1],biases[1:],flats[:-1],flats[1:]):
 		ff = [fitsio.FITS(f) for f in files]
 		data = np.empty(1,dtype=[('bias1','S15'),('bias2','S15'),
@@ -31,15 +32,21 @@ def calc_gain_rdnoise(biases,flats,margin=500):
 		data['bias2'] = os.path.basename(files[1])
 		data['flat1'] = os.path.basename(files[2])
 		data['flat2'] = os.path.basename(files[3])
+		skip = False
 		for ext in range(1,17):
-			bias1,bias2 = [ _data2arr(f[ext].read()[y1:y2,x1:x2],500,5000) 
+			bias1,bias2 = [ _data2arr(f[ext].read()[s],100,5000) 
 			                  for f in ff[:2]]
-			flat1,flat2 = [ _data2arr(f[ext].read()[y1:y2,x1:x2],500,40000) 
+			flat1,flat2 = [ _data2arr(f[ext].read()[s],100,50000) 
 			                  for f in ff[2:] ]
 			_B1 = bias1.mean()
 			_B2 = bias2.mean()
 			_F1 = flat1.mean()
 			_F2 = flat2.mean()
+			if ext==1 and (np.abs(_F1-_F2)/_F1) > 0.05:
+				# a very large jump in the flat field value will throw
+				# this calculation off, restrict it to 5% variation
+				skip = True
+				break
 			try:
 				varF1F2 = (flat1-flat2).var()
 				varB1B2 = (bias1-bias2).var()
@@ -57,7 +64,8 @@ def calc_gain_rdnoise(biases,flats,margin=500):
 			data['flatRmsADU'][0,ext-1] = flat1.std()
 			data['gain'][0,ext-1] = gain
 			data['rdnoise'][0,ext-1] = rdnoise
-		rv.append(data)
+		if not skip:
+			rv.append(data)
 	return np.concatenate(rv)
 
 
@@ -256,18 +264,27 @@ def bias_drops():
 
 def nightly_checks(utdir,logdir,redo=False):
 	from bokpipe.bokobsdb import generate_log
+	print 'running nightly check on ',utdir
 	logf = os.path.join(logdir,'log_ut%s.fits' % os.path.basename(utdir))
 	if os.path.exists(logf):
 		# assume that if log file exists processing is already done
 		if not redo:
 			return
+		else:
+			# start the file over
+			log = fitsio.read(logf,1)
+			os.remove(logf)
+			logFits = fitsio.FITS(logf,'rw')
+			logFits.write(log)
 	else:
 		generate_log([utdir],logf)
-	logFits = fitsio.FITS(logf,'rw')
-	log = logFits[1].read()
+		logFits = fitsio.FITS(logf,'rw')
+		log = logFits[1].read()
+	imType = np.char.rstrip(log['imType'])
+	fileNames = np.char.rstrip(log['fileName'])
 	# find first bias sequence
 	biases = []
-	is_bias = (log['imType'] == 'zero')
+	is_bias = (imType == 'zero')
 	ii = np.where(is_bias)[0]
 	for _i,i in enumerate(ii):
 		for j in range(_i+1,len(ii)):
@@ -278,7 +295,7 @@ def nightly_checks(utdir,logdir,redo=False):
 			break
 	# find first flat sequence in any filter
 	flats = []
-	is_flat = (log['imType'] == 'flat')
+	is_flat = (imType == 'flat')
 	ii = np.where(is_flat)[0]
 	for _i,i in enumerate(ii):
 		for j in range(_i+1,len(ii)):
@@ -292,8 +309,8 @@ def nightly_checks(utdir,logdir,redo=False):
 	#
 	# empirical gain/readnoise calculation
 	#
-	biases = [ os.path.join(utdir,f+'.fits') for f in log['fileName'][biases] ]
-	flats = [ os.path.join(utdir,f+'.fits') for f in log['fileName'][flats] ]
+	biases = [ os.path.join(utdir,f+'.fits') for f in fileNames[biases] ]
+	flats = [ os.path.join(utdir,f+'.fits') for f in fileNames[flats] ]
 	if len(biases)>3 and len(flats)>3:
 		# skip the first image in each sequence
 		gainrdnoise = calc_gain_rdnoise(biases[1:],flats[1:])
@@ -318,6 +335,100 @@ def nightly_checks(utdir,logdir,redo=False):
 	# finish up
 	logFits.close()
 
+def _reportfig_init8():
+	fig = plt.figure(figsize=(7.5,9.5))
+	fig.subplots_adjust(0.1,0.025,0.95,0.975,0.35,0.3)
+	return fig
+
+def gainrn_report(data,outf,utbreaks=None,save=True):
+	plot_fields = [ ('biasADU',(700,1200), 'biasRmsADU',(2,10)), 
+	                ('flatADU',(1e4,6e4), 'flatRmsADU',(0,600)), 
+	                ('gain',(0,3), 'rdnoise',(0,15)), 
+	]
+	for i in range(len(data)):
+		outf.write('%4d %s %s %s %s\n' % (i,data['bias1'][i],data['bias2'][i],
+		                                  data['flat1'][i],data['flat2'][i]))
+	outf.write('\n')
+	for fields in plot_fields:
+		fig = _reportfig_init8()
+		f1,fr1,f2,fr2 = fields
+		for i in range(4):
+			ax1 = fig.add_subplot(4,2,i+1)
+			ax2 = fig.add_subplot(4,2,i+1+4)
+			for utb in utbreaks:
+				for ax in [ax1,ax2]:
+					ax.axvline(utb,c='gray',ls=':',lw=0.5)
+			for j in range(4):
+				ax1.plot(data[f1][:,4*i+j],label='IM%d'%ampOrder[4*i+j])
+				ax2.plot(data[f2][:,4*i+j],label='IM%d'%ampOrder[4*i+j])
+			ax1.legend(ncol=4,frameon=False,fontsize=9,columnspacing=1.0)
+			ax2.legend(ncol=4,frameon=False,fontsize=9,columnspacing=1.0)
+			ax1.set_ylim(*fr1)
+			ax2.set_ylim(*fr2)
+			ax1.set_ylabel(f1+' CCD%d'%(i+1),size=11)
+			ax2.set_ylabel(f2+' CCD%d'%(i+1),size=11)
+		for f in [f1,f2]:
+			for j in range(4):
+				outf.write('%-12s CCD%d\n' % (f,j+1))
+				for i in range(len(data)):
+					outf.write('%4d  ' % (i))
+					for k in range(4):
+						outf.write('%8.2f '%data[f][i,4*j+k])
+					outf.write('\n')
+				outf.write('\n')
+		if save:
+			plt.savefig('bass_summary_%s.png'%f1[:4])
+
+def _reportfig_init16():
+	fig = plt.figure(figsize=(7.5,9.5))
+	fig.subplots_adjust(0.1,0.025,0.95,0.975,0.35,0.3)
+	return fig
+
+def bit_report(data,outf,utbreaks=None,save=True):
+	fig = _reportfig_init16()
+	for i in range(16):
+		ax = fig.add_subplot(4,4,i+1)
+		if utbreaks is not None:
+			for utb in utbreaks:
+				ax.axvline(utb,c='gray',ls=':',lw=0.5)
+		haslab = False # suppresses a warning message
+		for j in range(6):
+			l = 'bit%d'%j if j//2==i else None
+			if l is not None: haslab = True 
+			ax.plot(data['bitFreq'][:,i,j],label=l)
+		if haslab:
+			ax.legend(ncol=2,frameon=False,fontsize=8,columnspacing=1.0)
+		ax.set_ylim(0.35,0.65)
+		ax.yaxis.set_minor_locator(ticker.MultipleLocator(0.01))
+	if save:
+		plt.savefig('bass_summary_%s.png'%'bits')
+
+def nightly_report(logf):
+	fits = fitsio.FITS(logf)
+	data = fits[2].read()
+	gainrn_report(data)
+
+def combined_report(logdir):
+	logfiles = sorted(glob(os.path.join(logdir,'log_*.fits')))
+	data1,data2 = [],[]
+	utbreaks1,utbreaks2 = [0,],[0,]
+	for i,logf in enumerate(logfiles):
+		fits = fitsio.FITS(logf)
+		try:
+			data1.append(Table(fits[2].read()))
+			utbreaks1.append(len(data1[-1])+utbreaks1[-1])
+		except:
+			pass
+		try:
+			data2.append(Table(fits[3].read()))
+			utbreaks2.append(len(data2[-1])+utbreaks2[-1])
+		except:
+			pass
+	data1 = vstack(data1)
+	data2 = vstack(data2)
+	with open('bass_summary.txt','w') as outf:
+		gainrn_report(data1,outf,utbreaks1[1:])
+		bit_report(data2,outf,utbreaks2[1:])
 
 
 if __name__=='__main__':
@@ -327,26 +438,31 @@ if __name__=='__main__':
 	#linearity_check()
 	#bias_check()
 	#bias_drops()
+	plt.ioff()
 	parser = argparse.ArgumentParser()
 	parser.add_argument("-n","--nightly",action='store_true',
 	                    help="run nightly processing")
+	parser.add_argument("-r","--report",action='store_true',
+	                    help="make nightly report")
 	parser.add_argument("-d","--datadir",type=str,
 	                    default="/data/primefocus/bass/",
 	                    help="top-level data directory")
-	parser.add_argument("-o","--output",type=str,
+	parser.add_argument("-l","--logdir",type=str,
 	                    default="/home/mcgreer/basslogs/",
-	                    help="output directory")
-	parser.add_argument("-r","--redo",action='store_true',
+	                    help="log file directory")
+	parser.add_argument("-R","--redo",action='store_true',
 	                    help="ignore existing data and redo")
 	parser.add_argument("-u","--utdate",type=str,
 	                    help="restrict UT date")
 	args = parser.parse_args()
 	#
+	if args.utdate is not None:
+		utdirs = sorted(glob(os.path.join(args.datadir,args.utdate)))
+	else:
+		utdirs = sorted(glob(os.path.join(args.datadir,'201?????')))
 	if args.nightly:
-		if args.utdate is not None:
-			utdirs = [os.path.join(args.datadir,args.utdate)]
-		else:
-			utdirs = sorted(glob(os.path.join(args.datadir,'201?????')))
 		for utdir in utdirs:
-			nightly_checks(utdir,args.output,redo=args.redo)
+			nightly_checks(utdir,args.logdir,redo=args.redo)
+	if args.report:
+		combined_report(args.logdir)
 
