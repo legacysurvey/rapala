c34f69b34739a5bed3f9b37c2ca0d68fda6852c8
Ian McGreer <imcgreer@gmail.com>
2016-09-23 18:12:49 -0700

    fix cl args

--- bokpipe/tools/bokimarith.py
+++ bokpipe/tools/bokimarith.py
@@ -12,7 +12,7 @@ parser.add_argument("operator",type=str,
                     help="mathematical operator (+-x/)")
 parser.add_argument("operand",type=str,
                     help="image or value used as operand")
-parser.add_argument("outfile",nargs='?',type=str,
+parser.add_argument("-o","--output",type=str,
                     help="output image name (default is same)")
 parser.add_argument("-p","--prefix",type=str,default='',
                     help="prefix for output file names")
@@ -20,8 +20,13 @@ parser.add_argument("-s","--suffix",type=str,default='',
                     help="suffix for output file names")
 args = parser.parse_args()
 
-def namemap(f):
+def renamemap(f):
 	return args.prefix+f.replace('.fits','')+args.suffix+'.fits'
+
+if args.output:
+	namemap = lambda f: args.output
+else:
+	namemap = renamemap
 
 imarith = bokproc.BokImArith(args.operator,args.operand,output_map=namemap)
 
