introduced bug by not normalizing illum flat; minor fixes

bokpipe/bokpl.py
@@ -85,6 +85,7 @@ def _flat_worker(dataMap,bias2Dsub,flatStack,normFlat,nSkip,writeccdim,
 		bias2Dsub.process_files(flatList)
 	flatStack.stack(flatList[nSkip:],flatFile)
 	if normFlat:
+		raise NotImplementedError # currently all name mappings are broken
 		if debug:
 			shutil.copy(flatFile,
 			            flatFile.replace('.fits','_raw.fits'))
@@ -273,7 +274,8 @@ def make_illumcorr_image(dataMap,byUtd=False,max_images=None,**kwargs):
 		                           mask_file=dataMap.getCalMap('badpix4'),
 		                           read_only=True)
 		illum = bokproc.SplineBackgroundFit(fits,nKnots=7,order=3,nbin=8)
-		illum.write(outFn,clobber=True)
+		normFun = lambda arr: arr / np.float32(illum(0,0))
+		illum.write(outFn,opfun=normFun,clobber=True)
 
 def make_fringe_masters(dataMap,byUtd=False,**kwargs):
 	caldir = dataMap.getCalDir()
@@ -588,7 +590,8 @@ def init_data_map(args,create_dirs=True):
 		dataMap.setImageType(args.imtype)
 	#
 	if create_dirs:
-		for d in [dataMap.procDir,dataMap.getCalDir(),dataMap.getDiagDir()]:
+		for d in [dataMap.procDir,dataMap.getCalDir(),dataMap.getDiagDir(),
+		          dataMap.getTmpDir()]:
 			if not os.path.exists(d):
 				os.makedirs(d)
 		for _utdir in dataMap.getUtDirs():

bokpipe/bokproc.py
@@ -76,8 +76,10 @@ class BokImStat(bokutil.BokProcess):
 		self.clipArgs = kwargs.get('clip_args',{})
 		self.quickprocess = kwargs.get('quickprocess',False)
 		self.meanVals = []
+		self.rmsVals = []
 	def _preprocess(self,fits,f):
 		self.imgMeans = []
+		self.imgStds = []
 	def process_hdu(self,extName,data,hdr):
 		if self.quickprocess:
 			pix = overscan_subtract(data,hdr,method='mean_value',
@@ -85,16 +87,20 @@ class BokImStat(bokutil.BokProcess):
 			                        apply_filter=None)
 		else:
 			pix = data
-		v = bokutil.array_stats(pix[self.statSec],method='mean',
-		                        rms=False,clip=True,**self.clipArgs)
+		v,s = bokutil.array_stats(pix[self.statSec],method='mean',
+		                          rms=True,clip=True,**self.clipArgs)
 		self.imgMeans.append(v)
+		self.imgStds.append(s)
 		return data,hdr
 	def _postprocess(self,fits,f):
 		self.meanVals.append(self.imgMeans)
+		self.rmsVals.append(self.imgStds)
 	def _finish(self):
 		self.meanVals = np.array(self.meanVals)
+		self.rmsVals = np.array(self.rmsVals)
 	def reset(self):
 		self.meanVals = []
+		self.rmsVals = []
 
 def interpolate_masked_pixels(data,along='twod',method='linear'):
 	if along=='rows':
@@ -140,11 +146,14 @@ class BackgroundFit(object):
 		self.coordSys = coordsys
 	def __call__(self,extn):
 		raise NotImplementedError
-	def write(self,outFile,clobber=False):
+	def write(self,outFile,opfun=None,clobber=False):
 		outFits = fitsio.FITS(outFile,'rw',clobber=clobber)
 		outFits.write(None,header=self.fits.get_header(0))
 		for extn,data,hdr in self.fits:
-			outFits.write(self.get(extn),extname=extn,header=hdr)
+			im = self.get(extn)
+			if opfun:
+				im = opfun(im)
+			outFits.write(im.astype(np.float32),extname=extn,header=hdr)
 		outFits.close()
 
 class SplineBackgroundFit(BackgroundFit):

bokpipe/tools/bokimstat.py
@@ -13,6 +13,8 @@ parser.add_argument("inputFiles",type=str,nargs='+',
                     help="input FITS images")
 parser.add_argument("-e","--ext",type=str,default="all",
                     help="select FITS extension(s) [default=all]")
+parser.add_argument("-s","--statsreg",type=str,
+                    help="image region to calculate stats on")
 parser.add_argument("--oscan",action="store_true",
                     help="do a simple overscan subtraction")
 args = parser.parse_args()
@@ -22,12 +24,13 @@ if args.ext=='all':
 else:
 	extns = args.ext.split(',')
 
-imstat = BokImStat(extensions=extns,quickprocess=args.oscan)
+imstat = BokImStat(extensions=extns,quickprocess=args.oscan,
+                   stats_region=args.statsreg)
 imstat.process_files(args.inputFiles)
 
 np.set_printoptions(precision=2,suppress=True)
 
 for i,fn in enumerate(args.inputFiles):
-	print os.path.basename(fn),imstat.meanVals[i]
+	print os.path.basename(fn),imstat.meanVals[i],imstat.rmsVals[i]
 
 