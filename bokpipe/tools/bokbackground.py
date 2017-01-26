a0b6e5298f63ea16329574446e23aee81b27a21a
Ian McGreer <imcgreer@gmail.com>
2017-01-26 09:04:57 -0700

    add some options

--- bokpipe/tools/bokbackground.py
+++ bokpipe/tools/bokbackground.py
@@ -10,12 +10,14 @@ import argparse
 
 class CCDMedianBackgroundFit(bokproc.BackgroundFit):
 	def __init__(self,fits,**kwargs):
+		self.statsReg = kwargs.pop('stats_reg','ccd_central_quadrant')
+		self.method = kwargs.pop('method','median')
 		super(CCDMedianBackgroundFit,self).__init__(fits,**kwargs)
-		self.statsReg = stats_region('ccd_central_quadrant')
+		self.statsPix = stats_region(self.statsReg)
 		self.medBack = {}
 		for extn,data,hdr in fits:
-			self.medBack[extn] = array_stats(data[self.statsReg],
-			                                 method='median',clip=True)
+			self.medBack[extn] = array_stats(data[self.statsPix],
+			                                 method=self.method,clip=True)
 			self.imShape = data.shape
 	def get(self,extn):
 		return self.medBack[extn] + np.zeros(self.imShape,dtype=np.float32)
@@ -24,7 +26,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("input",type=str,
                     help="input FITS image")
 parser.add_argument("-f","--fit",type=str,default='spline',
-                    help="fit method (poly|[spline])")
+                    help="fit method (poly|[spline]|ccdmedian|ccdmean|ccdmode)")
 parser.add_argument("--order",type=int,default=1,
                     help="fit order [default=1]")
 parser.add_argument("--knots",type=int,default=2,
@@ -35,6 +37,8 @@ parser.add_argument("-o","--output",type=str,
                     help="output FITS image")
 parser.add_argument("-m","--mask",type=str,
                     help="mask FITS image")
+parser.add_argument("-s","--statsreg",type=str,default='ccd_central_quadrant',
+                    help="region to use for statistics (mean/median/mode)")
 parser.add_argument('-v','--verbose',action='count',
                     help='increase output verbosity')
 args = parser.parse_args()
@@ -50,7 +54,8 @@ elif args.fit == 'poly':
 	backfit = bokproc.PolynomialBackgroundFit(fits,
 	                                          order=args.order,nbin=args.nbin)
 elif args.fit == 'ccdmedian':
-	backfit = CCDMedianBackgroundFit(fits)
+	backfit = CCDMedianBackgroundFit(fits,stats_reg=args.statsreg,
+	                                 method=args.fit[3:])
 else:
 	raise ValueError
 
--- bokpipe/tools/bokcombine.py
+++ bokpipe/tools/bokcombine.py
@@ -22,20 +22,27 @@ parser.add_argument("-o","--output",type=str,default="bokstack.fits",
                     help="output image")
 parser.add_argument("-m","--method",type=str,default="mean",
                     help="stacking method (mean|median|mode)")
-parser.add_argument("-s","--scale",type=str,default="normalize_median",
+parser.add_argument("-s","--scale",action='store_true',
+#                    default="normalize_median",
                     help="scale images before combining")
-parser.add_argument("-r","--reject",type=str,default="sigma_clip",
+parser.add_argument("-r","--reject",action='store_true',
+#                    default="sigma_clip",
                     help="apply rejection")
-parser.add_argument("-n","--nsplit",type=int,default=8,
-                    help="number of chunks to split images into")
+parser.add_argument("--maxmem",type=int,default=8,
+                    help="maximum memory in GB")
 parser.add_argument("-w","--weights",type=str,nargs='+',
                     help="weight images")
+parser.add_argument('-v','--verbose',action='count',
+                    help='increase output verbosity')
 args = parser.parse_args()
 
 stackPars = {}
-stackPars['scale'] = args.scale
-stackPars['reject'] = args.reject
-stackPars['nsplit'] = args.nsplit
+if args.scale:
+	stackPars['scale'] = 'normalize_mean'
+if args.reject:
+	stackPars['reject'] = 'sigma_clip'
+stackPars['maxmem'] = args.maxmem
+stackPars['verbose'] = args.verbose
 
 if args.method == 'mean':
 	stackFun = bokutil.ClippedMeanStack(**stackPars)
@@ -45,9 +52,12 @@ elif args.method == 'median':
 inputFiles = _read_file_list(args.inputFiles)
 print 'input: ',inputFiles
 print 'output: ',args.output
+print stackPars
 if args.weights is not None:
 	weightFiles = _read_file_list(args.weights)
 	print 'weights: ',weightFiles
+else:
+	weightFiles = None
 
 stackFun.stack(inputFiles,args.output,weights=weightFiles)
 
