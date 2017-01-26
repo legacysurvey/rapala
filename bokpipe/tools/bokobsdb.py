183904db8bb858c22058d1ec9b03f7a16e57310a
Ian McGreer <imcgreer@gmail.com>
2017-01-26 10:52:56 -0700

    add some basic selection from db

--- bokpipe/tools/bokobsdb.py
+++ bokpipe/tools/bokobsdb.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import numpy as np
 from astropy.table import Table
 
 import argparse
@@ -7,11 +8,30 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("obsdb",type=str,
                     help="input observations database")
+parser.add_argument('-b','--band',type=str,default=None,
+                help='band to include [default=both]')
+parser.add_argument('--frames',type=str,default=None,
+                help='frames to include (i1,i2) [default=all]')
+parser.add_argument('-t','--imtype',type=str,default=None,
+                help='specify image type to include')
+parser.add_argument('-u','--utdate',type=str,default=None,
+                help='UT date(s) to include [default=all]')
+parser.add_argument('--night',type=str,default=None,
+                help='night(s) to include [default=all]')
 args = parser.parse_args()
 
 fields = ['frameIndex','fileName','imType',
           'filter','objName','expTime']
 
 obsdb = Table.read(args.obsdb)
-obsdb[fields].more()
+
+frames = np.ones(len(obsdb),dtype=bool)
+if args.band:
+	frames &= np.in1d(obsdb['filter'],args.band)
+if args.imtype:
+	frames &= np.in1d(obsdb['imType'],args.imtype)
+if args.utdate:
+	frames &= np.in1d(obsdb['utDate'],args.utdate)
+
+obsdb[frames][fields].more()
 
